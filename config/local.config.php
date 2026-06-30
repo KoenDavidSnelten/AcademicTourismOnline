6b5e23b279fc6fac93261f0b1baf37f9fa199dc6
Koen Snelten <koensnelten@gmail.com>
2026-06-30 22:35:23 +0200

    Initial commit

---
+++ config/local.config.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    'logger' => [
+        'log' => false,
+        'priority' => \Laminas\Log\Logger::NOTICE,
+    ],
+    'http_client' => [
+        'sslcapath' => null,
+        'sslcafile' => null,
+    ],
+    'cli' => [
+        'phpcli_path' => 'C:\\xampp\\php\\php.exe',
+    ],
+    'thumbnails' => [
+        'types' => [
+            'large' => ['constraint' => 800],
+            'medium' => ['constraint' => 200],
+            'square' => ['constraint' => 200],
+        ],
+        'thumbnailer_options' => [
+            'imagemagick_dir' => 'C:\\Program Files\\ImageMagick-7.1.2-Q16-HDRI\\convert.exe',
+        ],
+    ],
+    'translator' => [
+        'locale' => 'en_US',
+    ],
+    'service_manager' => [
+        'aliases' => [
+            'Omeka\File\Store' => 'Omeka\File\Store\Local',
+            'Omeka\File\Thumbnailer' => 'Omeka\File\Thumbnailer\Gd',
+            'Omeka\Job\DispatchStrategy' => 'Omeka\Job\DispatchStrategy\Synchronous',
+        ],
+    ],
+];
